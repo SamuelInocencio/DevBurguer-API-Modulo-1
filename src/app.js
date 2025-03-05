dda61dd3459fa62352c4b51cc0f54de1b6d58249
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-03-05 16:03:41 -0300

    commit data 05/03

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 class App {
   constructor() {
     this.app = express();
@@ -18,5 +20,3 @@ class App {
 }
 
 export default new App().app;
-
-
