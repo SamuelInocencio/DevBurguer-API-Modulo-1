c5f29e219e9ab884a1c990dab7d3cc9a53b94093
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-03-10 17:30:02 -0300

    commmit do dia 10/03

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,26 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import authMiddleware from './middlewares/auth';
 
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import ProductController from './app/controller/ProductController';
+import CategoryController from './app/controller/CategoryController';
 
 const routes = new Router();
 
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
+
+routes.post('/categories', CategoryController.store);
+routes.get('/categories', CategoryController.index);
 
 export default routes;
